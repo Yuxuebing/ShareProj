f2144b8d59fe1abc37bcc0dde608dea32cffe4aa
Yuxb <Yuxuebing201034@163.com>
2020-03-04 22:07:04 +0800

    初始化

--- share-home/src/main.js
+++ share-home/src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import Index from './Index'
 import router from './router'
 
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
