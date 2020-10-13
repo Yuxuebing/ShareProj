75c4ce41ca669a36e01efd949819d9e2d6365fcb
Yuxuebing <Yuxuebing201034@163.com>
2020-10-13 14:56:46 +0800

    管理后台页面框架

---
+++ share-admin/src/main/java/pers/yxb/share/AdminApplication.java
@@ -0,0 +1,13 @@
+package pers.yxb.share;
+
+/**
+ * Hello world!
+ *
+ */
+public class AdminApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
